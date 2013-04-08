8c725a0ce238475c5795b1f27474bc5ff00f379a
unknown <wanyang@wanyang-pc.(none)>
2013-04-08 13:50:31 +0800

    diyici tijiao
    sadasad

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
+
+echo '<strong>Welcome to MyClass </strong><br/>';
+
+
+echo "这里就是我们的域名测试地址了。<a href=\"http://pclass.sinaapp.com/\">MyClass</a></br>";
+echo " 计算机学院 10计科6班  ".date('Y-m-d');
+
+?>
